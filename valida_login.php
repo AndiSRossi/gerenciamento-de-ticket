e8bff03211ef79f1225adafdb6ff27534b77e968
Anderson Silva Rossi <andersonsrossi@gmail.com>
2025-06-10 22:09:20 -0300

    alterado o arquivo valida_login.php para corrigir o bug de login

--- valida_login.php
+++ valida_login.php
@@ -8,7 +8,7 @@ $senha = $_POST['senha'] ?? '';
 
 if (valida_login($login, $senha)) {
     $_SESSION['usuario'] = $login;
-    header("Location: pages/inicial.php");
+    header("Location: pages/conteudo/inicio_conteudo.php");
     exit();
 } else {
     header("Location: index.php?erro=1");
